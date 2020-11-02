2e5287ee8e7d9dcb9087c5b19c333c7c8053a79f
Eduard <edshav@protonmail.com>
2020-11-02 22:41:39 +0200

    add actions

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,6 +1,15 @@
 import { types } from "mobx-state-tree";
 
-export const Todo = types.model({
-  name: types.string,
-  done: false,
-});
+export const Todo = types
+  .model({
+    name: types.string,
+    done: false,
+  })
+  .actions((self) => ({
+    setName(newName: string): void {
+      self.name = newName;
+    },
+    toggle(): void {
+      self.done = !self.done;
+    },
+  }));
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,16 @@
 import { types } from "mobx-state-tree";
 import { Todo, User } from "./models";
 
-export const RootStore = types.model({
-  users: types.map(User),
-  todos: types.optional(types.map(Todo), {}),
-});
+export const RootStore = types
+  .model({
+    users: types.map(User),
+    todos: types.optional(types.map(Todo), {}),
+  })
+  .actions((self) => ({
+    addTodo(id: string, name: string) {
+      self.todos.set(id, Todo.create({ name }));
+    },
+  }));
 
 export const store = RootStore.create({
   users: {},
